69139c85f28790ccb98dc9cdc84bf217826ec84c
arielvitali <arielgonzalezvitali@hotmail.com>
2022-07-21 17:20:12 -0300

    more modules

--- app.js
+++ app.js
@@ -4,8 +4,10 @@
 
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-granade')
 
 
-sayHi('susan')
+/* sayHi('susan')
 sayHi(names.name)
-sayHi(names.surname)
+sayHi(names.surname) */
