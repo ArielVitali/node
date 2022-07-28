1f8158fdb0e6c52b5f042fba09901a31829145e2
arielvitali <arielgonzalezvitali@hotmail.com>
2022-07-28 15:03:37 -0300

    update

--- app.js
+++ app.js
@@ -7,3 +7,14 @@
 // global dependency - use it in any project
 // npm install -g <packageName>
 // sudo install -g <packageName> (mac)
+// uninstall = sacarlo del package.json y correr npm install
+
+//nodemon = npm run dev
+
+var _ = require('lodash')
+
+var items = [1,[2,[3,4]]]
+var newItems = _.flattenDeep(items)
+console.log(newItems)
+console.log('hello')
+
